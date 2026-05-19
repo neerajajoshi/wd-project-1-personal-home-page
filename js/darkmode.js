61c6289fa4ed3d5b11d8631bfa7e2d19729ce28a
catherine-han-neu <smokieunderscore777@gmail.com>
2026-05-19 17:43:56 -0400

    Add eslint config and lint

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,10 +2,10 @@
  * This JS script enables a dark mode functionality.
  * If an HTML file includes this script, it will gain a floating dark mode toggle button,
  * which when pressed, will toggle the page between dark and light modes.
- * The user's dark/light mode preference will be stored in the browser's localStorage API.
+ * The user"s dark/light mode preference will be stored in the browser"s localStorage API.
  */
 
-const darkModeCss = document.createElement('style');
+const darkModeCss = document.createElement("style");
 darkModeCss.textContent = `
   .dark_mode_toggle {
     position: fixed;
@@ -22,25 +22,25 @@ darkModeCss.textContent = `
 `;
 document.head.appendChild(darkModeCss);
 
-const darkModeToggleEl = document.createElement('button');
-darkModeToggleEl.className = 'dark_mode_toggle';
-darkModeToggleEl.type = 'button';
-darkModeToggleEl.textContent = 'Toggle Dark Mode';
+const darkModeToggleEl = document.createElement("button");
+darkModeToggleEl.className = "dark_mode_toggle";
+darkModeToggleEl.type = "button";
+darkModeToggleEl.textContent = "Toggle Dark Mode";
 document.body.appendChild(darkModeToggleEl);
 
-if (localStorage.getItem('is_dark_mode') == 'true') {
-  document.body.classList.add('dark_mode');
-  darkModeToggleEl.textContent = 'Toggle Light Mode';
+if (localStorage.getItem("is_dark_mode") == "true") {
+  document.body.classList.add("dark_mode");
+  darkModeToggleEl.textContent = "Toggle Light Mode";
 }
 
-darkModeToggleEl.addEventListener('click', () => {
-  document.body.classList.toggle('dark_mode');
-  const isDarkMode = document.body.classList.contains('dark_mode');
+darkModeToggleEl.addEventListener("click", () => {
+  document.body.classList.toggle("dark_mode");
+  const isDarkMode = document.body.classList.contains("dark_mode");
   if (isDarkMode) {
-    localStorage.setItem('is_dark_mode', 'true');
-    darkModeToggleEl.textContent = 'Toggle Light Mode';
+    localStorage.setItem("is_dark_mode", "true");
+    darkModeToggleEl.textContent = "Toggle Light Mode";
   } else {
-    localStorage.setItem('is_dark_mode', 'false');
-    darkModeToggleEl.textContent = 'Toggle Dark Mode';
+    localStorage.setItem("is_dark_mode", "false");
+    darkModeToggleEl.textContent = "Toggle Dark Mode";
   }
 });
